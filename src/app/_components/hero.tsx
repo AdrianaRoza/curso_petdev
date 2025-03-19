9729b936d2de28f9305614ce8fd71829a50e002c
AdrianaRoza <adriana.roza.aka@gmail.com>
2025-03-19 14:59:37 -0400

    first commit

---
+++ src/app/_components/hero.tsx
@@ -0,0 +1,11 @@
+
+
+const Hero = () => {
+  return(
+    <div>
+      <h1>HERO COMPONENT</h1>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Hero from "./_components/hero"
+
+
+const Home = () => {
+  return(
+    <main>
+      <Hero />
+    </main>
+  )
+}
+
+export default Home
