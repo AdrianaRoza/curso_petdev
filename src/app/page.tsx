4dc3c8523e1924b568220840792dd3081852726c
AdrianaRoza <adriana.roza.aka@gmail.com>
2025-03-21 13:59:55 -0400

    services-carousel creation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import About from "./_components/about"
 import Hero from "./_components/hero"
+import Services from "./_components/services"
 
 
 const Home = () => {
   return(
     <main>
       <Hero />
-      <About/>
+      <About />
+      <Services />
     </main>
   )
 }
